1c30b37ad8e1c6f868eae2a42f38c6fd5fc6bfb3
Andrew Nicolaou <andrew.nicolaou@bbc.co.uk>
2015-07-22 15:44:53 +0100

    Remove unusued readline parser

--- espruino/source.js
+++ espruino/source.js
@@ -66,26 +66,6 @@ function isArraySame(ar1, ar2) {
   return true;
 }
 
-// From: https://github.com/voodootikigod/node-serialport/blob/master/parsers.js#L11-L28
-// encoding: ascii utf8 utf16le ucs2 base64 binary hex
-// More: http://nodejs.org/api/buffer.html#buffer_buffer
-function readlineParser(delimiter, encoding) {
-  if (typeof delimiter === 'undefined' || delimiter === null) { delimiter = '\r'; }
-  if (typeof encoding  === 'undefined' || encoding  === null) { encoding  = 'utf8'; }
-  // Delimiter buffer saved in closure
-  var data = '';
-  return function (emitter, buffer) {
-    // Collect data
-    data += buffer.toString(encoding);
-    // Split collected data by delimiter
-    var parts = data.split(delimiter);
-    data = parts.pop();
-    parts.forEach(function (part) {
-      emitter.emit('data', part);
-    });
-  };
-}
-
 //
 // This function is called by external
 // systems over the serial port
